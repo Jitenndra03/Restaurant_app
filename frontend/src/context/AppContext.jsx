57b4f4e8ba750ac1ea1d0d6166d32565bb4ce602
Aditya Srivastava <149177246+addysrii@users.noreply.github.com>
2026-03-03 17:24:05 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -47,11 +47,13 @@ const AppContextProvider = ({ children }) => {
   /** Check if user is authenticated (restores session on page load) */
   const isAuth = async () => {
     try {
-      const token = localStorage.getItem(token)
+      const token = localStorage.getItem("token")
       console.log("token is", token)
-      const { data } = await axios.get("/api/auth/is-auth",{
-         headers: {Authorization: `Bearer ${token}` },
-      });
+      const { data } = await axios.get("/api/auth/is-auth", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
       });
       if (data.success) {
